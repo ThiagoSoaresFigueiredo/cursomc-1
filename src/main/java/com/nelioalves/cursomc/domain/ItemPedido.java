aee2b2cf597c8e814545edede44cdcb7b4b5e2a5
Nelio Alves <acenelio@gmail.com>
2017-09-21 20:55:36 -0300

    Trocando para JsonIgnore e incluindo POST de Pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -44,8 +44,12 @@ public class ItemPedido implements Serializable {
 		return id;
 	}
 
-	public void setId(ItemPedidoPK id) {
-		this.id = id;
+	public void setPedido(Pedido pedido) {
+		this.id.setPedido(pedido);
+	}
+	
+	public void setProduto(Produto produto) {
+		this.id.setProduto(produto);
 	}
 
 	public Double getDesconto() {
--- src/main/java/com/nelioalves/cursomc/services/PedidoService.java
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -3,7 +3,9 @@ package com.nelioalves.cursomc.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nelioalves.cursomc.domain.ItemPedido;
 import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.repositories.ItemPedidoRepository;
 import com.nelioalves.cursomc.repositories.PedidoRepository;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -12,6 +14,9 @@ public class PedidoService {
 	
 	@Autowired
 	private PedidoRepository repo;
+	
+	@Autowired 
+	private ItemPedidoRepository itemPedidoReposiroty;
 	
 	public Pedido buscar(Integer id) {
 		Pedido obj = repo.findOne(id);
@@ -22,4 +27,13 @@ public class PedidoService {
 		return obj;
 	}
 
+	public void inserir(Pedido obj) {
+		obj.setId(null);
+		repo.save(obj);
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setPedido(obj);
+		}
+		itemPedidoReposiroty.save(obj.getItens());
+	}
+	
 }
